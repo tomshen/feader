c02b47f89e3bdd90a82b69df09896154d9f245d9
Tom Shen <tom@tomshen.me>
2013-07-08 21:49:22 -0400

    Add article model, article-feed relation

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var express = require('express'),
     cons = require('consolidate'),
     path = require('path'),
     http = require('http'),
-    orm = require('orm');
+    orm = require('orm'),
+    _ = require('underscore');
 
 var config = require('./config');
 
@@ -17,7 +18,7 @@ app.use(express.logger());
 // assign the swig engine to .html files
 app.engine('html', cons.swig);
 
-// set .html as the default extension 
+// set .html as the default extension
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
@@ -30,34 +31,34 @@ app.configure(function () {
 app.use(orm.express(databaseURL, {
   define: function (db, models) {
     models.feed = db.define('feed', {
-      title: String,
-      description: String,
-      link: String,
-      xmlurl: String,
+      title: { type:'text', size: 4096 },
+      description: { type:'text', size: 4096 },
+      link: { type:'text', size: 4096 },
+      xmlurl: { type:'text', size: 4096 },
       date: Date,
       pubdate: Date,
-      author: String,
-      language: String,
-      favicon: String,
-      copyright: String
+      author: { type:'text', size: 4096 },
+      language: { type:'text', size: 4096 },
+      favicon: { type:'text', size: 4096 },
+      copyright: { type:'text', size: 4096 }
     });
 
     models.article = db.define('article', {
-      title: String,
-      description: String,
-      link: String,
+      title: { type:'text', size: 4096 },
+      description: { type:'text', size: 4096 },
+      link: { type:'text', size: 4096 },
       date: Date,
       pubdate: Date,
-      author: String,
-      guid: String
+      author: { type:'text', size: 4096 },
+      guid: { type:'text', size: 4096 }
     });
 
     models.account = db.define('account', {
       username: { type: 'text', required: true },
       password: { type: 'text', required: true },
-      firstname: String,
-      lastname: String,
-      email: String
+      firstname: { type:'text', size: 4096 },
+      lastname: { type:'text', size: 4096 },
+      email: { type:'text', size: 4096 }
       // TODO: add more account config options
     });
 
@@ -93,49 +94,110 @@ function getFeed(url, callback) {
     })
     .on('readable', function () {
       var item = this.read();
-      if(item) {
-        data.articles.push(item);
-        if(data.articles.length === 10)
-          callback(err, data);
-      } else callback(err, data);
-      
+      if(item) data.articles.push(item);
+    })
+    .on('end', function() {
+      callback(err, { meta: data.meta, articles: data.articles.slice(0, 10) });
     });
+}
+
+function handleError(err, res) {
+  if(err) {
+    console.error(err);
+    res.send(500);
+  }
 }
 
 app.get('/', function(req, res){
   res.render('index');
 });
 
-app.get('/feed', function(req, res){
+app.get('/feed', function(req, res) {
   getFeed(req.query.url, function(err, feed) {
-    if(err) res.send(err);
-    else res.render('feed', feed);
+    handleError(err, res);
+    res.render('feed', feed);
   });
 });
 
-app.get('/feed/new', function(req, res){
-  getFeed(req.query.url, function(err, feed) {
-    if(err) res.send(err);
-    else {
-      var feedData = feed.meta;
-      var articleDataArray = feed.articles;
-      req.models.feed.find({ xmlurl: feedData.xmlurl }, function(err, feeds) {
-        if(err) res.send(err);
-        else if(feeds.length > 0) {
-          req.models.feed.get(feeds[0].id, function(err, sameFeed) {
-            sameFeed.save(feedData, function(err) {
-              if(err) res.send(err);
-              else res.render('feed', feed);
+app.get('/feed/:id', function(req, res) {
+  req.models.feed.get(req.params.id, function(err, feed) {
+    handleError(err, res);
+    if(feed) res.send(feed);
+    else res.send(404);
+  });
+});
+
+app.post('/feed/new', function(req, res) {
+  getFeed(req.body.url, function(err, feed) {
+    handleError(err, res);
+    var feedData = feed.meta;
+    req.models.feed.find({ xmlurl: feedData.xmlurl }, function(err, feeds) {
+      handleError(err, res);
+      if(feeds.length > 0) {
+        req.models.feed.get(feeds[0].id, function(err, sameFeed) {
+          sameFeed.save(feedData, function(err) {
+            handleError(err, res);
+          });
+        });
+      } else {
+        req.models.feed.create(feedData, function(err, item) {
+          handleError(err, res);
+        });
+      }
+    });
+    res.send(200);
+  });
+});
+
+app.post('/feed/:id/update', function(req, res) {
+  req.models.feed.exists(req.params.id, function(err, exists) {
+    if(err) res.send(error);
+    if(!exists) res.send(404);
+  });
+  req.models.feed.get(req.params.id, function(err, feed) {
+    handleError(err);
+    if(!feed) res.send(404);
+    getFeed(feed.xmlurl, function(err, feed) {
+      handleError(err, res);
+      _(feed.articles).each(function(element, index, list) {
+        req.models.article.find({ guid: element.guid, link: element.link }, function(err, articles) {
+          handleError(err, res);
+          if(articles.length == 0) {
+            var newArticle = new req.models.article(element);
+            handleError(err, res);
+            req.models.feed.get(req.params.id, function(err, feed) {
+              handleError(err, res);
+              newArticle.setFeed(feed, function(err) {
+                handleError(err, res);
+                newArticle.save(function(err) {
+                  handleError(err, res);
+                  res.send(feed);
+                });
+              });
             });
-          });
-        } else {
-          req.models.feed.create(feedData, function(err, items) {
-            if(err) res.send(err);
-            else res.render('feed', feed);
-          });
-        }
+          }
+        });
       });
-    }
+    });
+  });
+});
+
+app.get('/feed/:feedId/article/:articleId', function(req, res) {
+  req.models.feed.get(req.params.feedId, function(err, feed) {
+    handleError(err, res);
+    if(feed) {
+      req.models.article.get(req.params.articleId, function(err, article) {
+        handleError(err, res);
+        if(article) article.getFeed(function(err, articleFeed) {
+          handleError(err, res);
+          if(articleFeed && articleFeed.id == feed.id) {
+            article.description = JSON.stringify(article.description);
+            res.send(article);
+          }
+        });
+        else res.send(404);
+      });
+    } else res.send(404);
   });
 });
 
